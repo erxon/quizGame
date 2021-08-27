66d71842a67482ce86325831fdef198f65987015
Ericson Castasus <ericsoncastasus@outlook.com>
2021-08-27 10:57:16 +0800

    get player name using body parser and render player name to quiz.ejs using ejs

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const bodyParser= require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs")
 
 app.get("/", function (req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
+app.post("/", function(req, res){
+  var playerName = req.body.name;
 
+  res.render("quiz", {name: playerName})
+})
 app.listen(3000, function (){
   console.log("Server is running on port 3000");
 });
